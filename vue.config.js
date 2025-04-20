8a346fa8138a5d67df6ec0625e2a3bdb7c1554ad
Farah bouhzam <148548537+1fa23@users.noreply.github.com>
2025-04-20 14:33:33 +0000

    changing

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    client: {
-      webSocketURL: {
-        protocol: 'wss',
-        hostname: 'stunning-broccoli-pjpvw96rpp6pc9w7-8080.app.github.dev', // your codespace hostname
-        port: 443,
-        pathname: '/ws',
-      },
-    },
-  },
-});
+    webSocketServer: false
+  }
+})
